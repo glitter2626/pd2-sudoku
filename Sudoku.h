52cd0425d15823ee1cec2d472340ddd63bc3280f
glitter2626 <gercommand@gmail.com>
2016-03-27 20:26:37 +0800

    7ver project1 for cpp class

--- Sudoku.h
+++ Sudoku.h
@@ -7,7 +7,8 @@
 #include<iterator>
 #include<algorithm>
 #include<cstdlib>
-#include<time>
+#include<ctime>
+#include<algorithm>
 
 using std::cin;
 using std::cout;
@@ -15,7 +16,7 @@ using std::vector;
 using std::set;
 using std::endl;
 
-int NUMBERS[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+int NUMBERS[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 int answer = 0;
 vector<int> ans1(81);
 
@@ -24,11 +25,12 @@ class Sudoku{
         typedef struct{
             int num;
             bool fixed;
-            set<int> candidators(NUMBERS, NUMBERS + 9);    
+            set<int> candidators;    
         }sudokuCell;
         
         Sudoku();
-        explict Sudoku(const int *arr);
+        Sudoku & operator=(Sudoku & a);
+        explicit Sudoku(const int *arr);
 
         void giveQuestion();
         void readIn();
@@ -40,30 +42,45 @@ class Sudoku{
         void flip(int n);
         void transform();
         
-        void findSudokuSolution(Sudoku & game, int sp);
+        void findSudokuSolution(int sp);
         bool setCandidatorTofixed(int sp, int trynum);
-        bool exlusiveCorrelativeCandidators(int sp, int trynum);
+        bool exclusiveCorrelativeCandidators(int sp, int trynum);
         bool processSinglesCandidature(int sp);
         void printSudokuSolution();
+        void validcandidators(int sp);
 
     private:
-        vector<sudokuCell> cell(81);
+        Sudoku(Sudoku &a){}
+        sudokuCell cell[81];
         int fixedcount;
 };
 
 Sudoku::Sudoku():fixedcount(0){}
 
+Sudoku & Sudoku::operator=(Sudoku & a){
+    fixedcount = a.fixedcount;
+    for(int i = 0; i < 81; i++){
+        cell[i].num = a.cell[i].num;
+        cell[i].fixed = a.cell[i].fixed;
+        cell[i].candidators.clear();
+        copy(a.cell[i].candidators.begin(), a.cell[i].candidators.end(), std::insert_iterator<set<int> >(cell[i].candidators, cell[i].candidators.begin()));
+    
+    }
+    return *this;
+}
+
 Sudoku::Sudoku(const int *arr):fixedcount(0){
     for(int i = 0; i < 81; i++){
         if(arr[i] != 0){
+            fixedcount++;
             cell[i].num = arr[i];
             cell[i].fixed = true;
-            cell[i].candidators.insert(arr[i]);
+            /*cell[i].candidators.insert(arr[i]);*/
         }
         else{
             cell[i].num = 0;
             cell[i].fixed = false;
-            cell[i].candidators.insert(NUMBERS, NUMBERS + 9);
+            cell[i].candidators.insert(NUMBERS, NUMBERS+9);
         }
         
     }
@@ -72,20 +89,22 @@ Sudoku::Sudoku(const int *arr):fixedcount(0){
 
 void Sudoku::readIn(){
     int a, i = 0;
-    cin>>a;
-    while(cin){
+    while(i < 81){
+        cin>>a;
         if(a != 0){
+            fixedcount++;
             cell[i].num = a;
             cell[i].fixed = true;
-            cell[i].candidators.insert(a);
+           /* cell[i].candidators.insert(a);*/
         }
         else{
             cell[i].num = 0;
             cell[i].fixed = false;
-            cell[i].candidators.insert(NUMBERS, NUMBERS + 9);
+            cell[i].candidators.insert(NUMBERS, NUMBERS+9);
         }
-        cin>>a;
-    }   
+        i++;
+    }
+    
 }
 
 void Sudoku::printSudokuSolution(){
@@ -94,6 +113,18 @@ void Sudoku::printSudokuSolution(){
             cout<<cell[i * 9 + j].num<<" ";
         cout<<endl;
     } 
+        std::ostream_iterator<int, char > ot(cout, " ");
+
+    for(int i = 0; i < 81; i++){
+        copy(cell[i].candidators.begin(), cell[i].candidators.end(), ot);
+        cout<<endl;
+    } 
+
+    for(int i = 0; i< 9; i++){
+    for(int j = 0; j < 9; j++)
+    cout<<cell[i*9 + j].fixed<<" ";
+    cout<<endl;
+    }
 }
 
 void Sudoku::findSudokuSolution(int sp){
@@ -104,11 +135,11 @@ void Sudoku::findSudokuSolution(int sp){
         return;
     }
 
-    if(fixedcount > 81)
-        exit(1);
-
-    if(cell[sp].fixed == true)
-        return (findSudokuSolution(sp + 1)); 
+    while(cell[sp].fixed == true){
+        sp++;
+        if(sp >= 81)
+            return(findSudokuSolution(sp));
+    }
 
     Sudoku newState;
     set<int>::iterator it = cell[sp].candidators.begin();
@@ -119,10 +150,8 @@ void Sudoku::findSudokuSolution(int sp){
 
         ++it;
 
-        if(answer == 2){
-            cout<<2<<endl;
+        if(answer == 2)
             return;
-        }
     }
     
 }
@@ -131,21 +160,27 @@ bool Sudoku::exclusiveCorrelativeCandidators(int sp, int trynum){
     
     int row = sp / 9;
     int col = sp % 9;
-    for( i = 0; i < 9; i++){
-        cell[row * 9 + i].candidators.erase(trynum);
-        if(cell[row * 9 + i].candidators.empty())
-            return false;
+    for(int i = 0; i < 9; i++){
+        if(!cell[row * 9 + i].fixed){
+            cell[row * 9 + i].candidators.erase(trynum);
+            if(cell[row * 9 + i].candidators.empty())
+                return false;
+        }
 
-        cell[col + 9 * i].candidators.erase(trynum);
-        if(cell[i + 9 * i].candidators.empty)
-            return false;
+        if(!cell[col + 9 * i].fixed){
+            cell[col + 9 * i].candidators.erase(trynum);
+            if(cell[col + 9 * i].candidators.empty())
+                return false;
+        }
     }
 
-    for( i = 0; i < 3; i++)
-        for( j = 0; j < 3; j++){
-            cell[(row / 3 + i) + j].candidators.erase(trynum);
-            if(cell[(row / 3 + i) + j].candidators.empty())
-                return false;
+    for(int i = 0; i < 3; i++)
+        for(int j = 0; j < 3; j++){
+            if(!cell[row / 3 + (col / 3) * 3 + i * 9 + j].fixed){
+                cell[row / 3 + ( col / 3 ) * 3 + i * 9 + j].candidators.erase(trynum);
+                if(cell[row / 3 + (col / 3) * 3 + i * 9 + j].candidators.empty())
+                    return false;
+            }
         }
 
     return true;
@@ -154,7 +189,7 @@ bool Sudoku::exclusiveCorrelativeCandidators(int sp, int trynum){
 bool Sudoku::processSinglesCandidature(int sp){
     int row = sp / 9;
     int col = sp % 9;
-    for( i = 0; i < 9; i++){
+    for(int i = 0; i < 9; i++){
         if(!cell[row * 9 + i].fixed && cell[row * 9 + i].candidators.size()==1)
             if(!setCandidatorTofixed(sp, *(cell[row * 9 + i].candidators.begin()            )))
                 return false;
@@ -164,10 +199,10 @@ bool Sudoku::processSinglesCandidature(int sp){
                 return false;
     }
 
-    for( i = 0; i < 3; i++)
-        for( j = 0; j < 3; j++){
-            if(!cell[(row / 3 + i) + j].fixed && cell[(row / 3 + i) + j].candidators.size()==1)
-                if(!setCandidatorTofixed(sp, *(cell[(row / 3 + i) + j].candidators.begin())))
+    for(int i = 0; i < 3; i++)
+        for(int j = 0; j < 3; j++){
+            if(!cell[row / 3 + (col / 3) * 3 + i * 9 + j].fixed && cell[row / 3 + (col / 3) * 3 + i * 9 + j].candidators.size()==1)
+                if(!setCandidatorTofixed(sp, *(cell[row / 3 + (col / 3) * 3 + i * 9 + j].candidators.begin())))
                 return false;
         }
 
@@ -177,21 +212,36 @@ bool Sudoku::processSinglesCandidature(int sp){
 
 bool Sudoku::setCandidatorTofixed(int sp, int trynum){
     
-    cell[sp].fixed = true;
-    cell[sp].num = trynum;
+   /* if(!validcandidators(sp, trynum))
+        return false;
+    else{ */   
+        cell[sp].fixed = true;
+        cell[sp].num = trynum;
+        cell[sp].candidators.clear();
+   /* } */
+
+
 
     if(!exclusiveCorrelativeCandidators(sp, trynum))
         return false;
     if(!processSinglesCandidature(sp))
         return false;
-
     fixedcount++;
+    cout<<sp<<" is "<<trynum<<endl;
     return true;    
 }
 
 void Sudoku::solve(){
     readIn();
+    std::ostream_iterator<int, char> ot(cout, " ");
+    for(int i = 0; i < 81; i++){
+        validcandidators(i);
+        copy(cell[i].candidators.begin(), cell[i].candidators.end(), ot);
+        cout<<endl;
+    }
     findSudokuSolution(0);
+    if(answer == 2)
+        cout<<2<<endl;
     if(answer == 0)
         cout<<0<<endl;
     if(answer == 1){
@@ -202,6 +252,24 @@ void Sudoku::solve(){
         }
     }                
 }
+
+void Sudoku::validcandidators(int sp){
+    for(int i = 0; i < 9; i++){
+        if(cell[(sp / 9 ) * 9 + i].fixed)
+            cell[sp].candidators.erase(cell[(sp / 9) * 9 + i].num);
+            
+        if(cell[(sp % 9) + 9 * i].fixed)
+            cell[sp].candidators.erase(cell[(sp % 9) + 9 * i].num);
+            
+    }
+    for(int i = 0; i < 3; i++)
+        for(int j = 0; j < 3; j++){
+            if(cell[(sp / 9 ) / 3 + ((sp % 9) / 3) * 3 +  i * 9 + j].fixed)
+                cell[sp].candidators.erase(cell[(sp / 9) / 3 + ((sp % 9) / 3) * 3 + i * 9 + j].num);
+        }
+    
+}
+
 
 void Sudoku::changeNum(int a, int b){
     int as, bs;
@@ -234,11 +302,11 @@ void Sudoku::changeNum(int a, int b){
 void Sudoku::changeRow(int a, int b){
     if( a >= 0 && a <= 2 && b >= 0 && b <= 2 && a != b){ 
      sudokuCell copy[27];
-    for(i = 0; i < 27; i++)
+    for(int i = 0; i < 27; i++)
         copy[i] = cell[ 27 * a + i];
-    for(i = 0; i < 27; i++)
+    for(int i = 0; i < 27; i++)
         cell[27 * a + i] = cell[27 * b + i];
-    for(i = 0; i < 27; i++)
+    for(int i = 0; i < 27; i++)
         cell[27 * b + i] = copy[i];    
     } 
 }
@@ -246,22 +314,22 @@ void Sudoku::changeRow(int a, int b){
 void Sudoku::changeCol(int a, int b){
     if( a >= 0 && a <= 2 && b >= 0 && b <= 2 && a != b){
     sudokuCell copy[27];
-    for(i = 0; i < 27; i++)
+    for(int i = 0; i < 27; i++)
         copy[i] = cell[i * 9 + a * 3];
-    for(i = 0; i < 27; i++)
+    for(int i = 0; i < 27; i++)
         cell[i * 9 + a * 3] = cell[i * 9 + b * 3];
-    for(i = 0; i < 27; i++)
+    for(int i = 0; i < 27; i++)
         cell[i * 9 + b * 3] = copy[i];
     }
 }
 
 void Sudoku::rotate(int n){
     int a;
-    sudokuCell copy(81);
+    Sudoku::sudokuCell copy[81];
     a = n % 4;
     switch(a){
         case 1 :
-            for(i = 0; i < 81; i++)
+            for(int i = 0; i < 81; i++)
                 copy[i] = cell[i];
 
             for(int i = 8; i >= 0; i--) 
@@ -271,17 +339,17 @@ void Sudoku::rotate(int n){
             break;
 
         case 2 :
-            for(i = 0; i < 81; i++)
+            for(int i = 0; i < 81; i++)
                 copy[i] = cell[i];
 
             for(int i = 8; i >= 0; i--)
                 for(int j = 8; j >= 0; j--){
-                    cell[i * 9 + j] = copy[(8 - j) + 9 * (8 - i)]
+                    cell[i * 9 + j] = copy[(8 - j) + 9 * (8 - i)];
                 }
             break;
 
         case 3 :
-            for(i = 0; i < 81; i++)
+            for(int i = 0; i < 81; i++)
                 copy[i] = cell[i];
 
             for(int i = 0; i < 9; i++)
@@ -296,29 +364,29 @@ void Sudoku::rotate(int n){
 void Sudoku::flip(int n){
     sudokuCell copy[81];
     if(n == 0){
-        for(i = 0; i < 81; i++)
+        for(int i = 0; i < 81; i++)
             copy[i] = cell[i];
         
-        for(i = 8; i > 4; i--)
-            for(j = 0; j < 9; j++)
+        for(int i = 8; i > 4; i--)
+            for(int j = 0; j < 9; j++)
                 cell[i * 9 + j] = copy[(8 - i) * 9 + j];
 
-        for(i = 8; i > 4; i--)
-            for(j = 0; j < 9; j++)
+        for(int i = 8; i > 4; i--)
+            for(int j = 0; j < 9; j++)
                 cell[(8 - i) * 9 + j] = copy[i * 9 + j];
 
     }
     
     if(n == 1){
-        for(i = 0; i < 81; i++)
+        for(int i = 0; i < 81; i++)
             copy[i] = cell[i];
 
-        for(i = 0; i < 4; i++)
-            for(j = 0; j < 9; j++)
+        for(int i = 0; i < 4; i++)
+            for(int j = 0; j < 9; j++)
                 cell[j * 9 + i] = copy[(8 - i) + j * 9];
 
-        for(i = 0; i < 4; i++)
-            for(j = 0; j < 9; j++)
+        for(int i = 0; i < 4; i++)
+            for(int j = 0; j < 9; j++)
                 cell[(8 - i) + j * 9] = copy[j * 9 + i];
     
     }
@@ -345,10 +413,10 @@ void Sudoku::giveQuestion(){
                       0, 0, 0, 1, 0, 0, 0, 3, 0,
                       0, 0, 1, 0, 0, 0, 0, 6, 8,
                       0, 0, 8, 5, 0, 0, 0, 1, 0,
-                      0, 9, 0, 0, 0, 0, 4, 0, 0 } 
+                      0, 9, 0, 0, 0, 0, 4, 0, 0 }; 
 
-    for(i = 0; i < 9; i++){
-        for(j = 0; j < 9; j++)
+    for(int i = 0; i < 9; i++){
+        for(int j = 0; j < 9; j++)
             cout<<array[i * 9 + j]<<" ";
         cout<<endl;
     }    
